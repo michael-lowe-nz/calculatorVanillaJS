92736c9eba45ba73924bd9b9165c2096147e8845
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-06-20 23:10:51 +1200

    Add base functionality of calculator

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-console.log('hello')
+document.getElementById('submit').addEventListener('click', addNumber)
 
 var numbers = []
 
-document.getElementById('submit').addEventListener('click', function() {
-  console.log('clicked')
-})
-
 function addNumber () {
-  console.log('adding')
-  numbers.push(document.getElementById('input').value)
-  var htmlNumbers = numbers.map(function (item) {
-    "<li>" + item + "</li>"
-  })
+  if (getNumber()) {
+    numbers.push(getNumber())
+    var newElement = createNumberListEl(getNumber())
+    document.getElementById('values').appendChild(newElement)
+    attachSum(sumOfArray(numbers))
+  }
+}
 
-  // document.getElementById('values').innerHTML = htmlNumbers
-  document.getElementById('values').innerHTML = "Hello World"
+function attachSum (n) {
+  document.getElementById('current').innerHTML = n
+}
+
+function getNumber () {
+  number = document.getElementById('input').value
+  if (number !== null) {
+    return Number(number)
+  }
+  else {
+    return null
+  }
+}
+
+function createNumberListEl (n) {
+  var newElement = document.createElement('li')
+  var content = document.createTextNode(n)
+  newElement.appendChild(content)
+  return newElement
+}
+
+function sumOfArray (arr) {
+  return arr.reduce(function(previous, current){
+    return previous + current
+  },0)
 }
