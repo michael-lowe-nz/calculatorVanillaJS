3bbf4e1c9ed09e689978da4a5630b6ac8602d655
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-06-26 23:48:43 +1200

    Fix 'clear' bug

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ function handleSubmit (e) {
 function handleClear (e) {
   e.preventDefault()
   numbers = []
-  node = document.getElementById('values')
+  console.log(document.getElementById('values'))
+  const node = document.getElementById('values')
   while (node.hasChildNodes()) {
     node.removeChild(node.lastChild)
   }
