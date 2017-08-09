2f2c8c31c18da72725e62fb18062351cdb0f2409
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-08-10 00:13:26 +1200

    Add redux for clear, but broken

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,14 +1,14 @@
 module.exports = (state, action) => {
-  const newState = Object.assign({}, state)
   switch (action.type) {
     case 'ADD_NUMBER':
-      newState.numbers.push(action.payload)
-      return newState
+      return Object.assign({}, state, {
+        numbers: [
+          ...state.numbers, action.payload
+        ]
+      })
     case 'CLEAR_NUMBERS':
-      console.log('Newstate', newState)
-      newState.numbers.splice(0, newState.numbers.length)
-      return newState
+      return Object.assign({})
     default:
-      return newState
+      return state
   }
 }
